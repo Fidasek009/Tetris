23a3bfc85d98416ae321a5f915ae9e51e0346779
fidasek009 <fidasek009@gmail.com>
2023-07-11 23:29:15 +0200

    quite tetris

--- Tetris.js
+++ Tetris.js
@@ -97,14 +97,18 @@ const SHAPES = [I, J, L, O, S, T, Z]
 const Shape = class {
     currentShape = []
     centerPos = {x: 0, y: 0}
+    field = []
 
-    constructor(fieldWidth) {
+    constructor(field) {
+        this.field = field
         this.shape = this.randomShape()
         console.log(this.shape)
         this.rotation = 0
 
         this.currentShape = this.shape.shapes[this.rotation]
-        this.centerPos = {x: Math.floor(fieldWidth/2), y: Math.floor(this.currentShape.length/2)}
+        this.centerPos = {x: Math.floor(field[0].length/2), y: Math.floor(this.currentShape.length/2)}
+
+        gameOver = this.checkCollision()
     }
 
     randomShape = () => SHAPES[Math.floor(Math.random() * SHAPES.length)]
@@ -114,49 +118,104 @@ const Shape = class {
         return {x: this.centerPos.x + (x - shapeCenter.x), y: this.centerPos.y + (y - shapeCenter.y)}
     }
 
+
+    checkCollision = () => {
+        for (let i = 0; i < this.currentShape.length; i++) {
+            for (let j = 0; j < this.currentShape[i].length; j++) {
+                // skip empty cells
+                if(this.currentShape[i][j] == 0) continue
+
+                let absPos = this.getPos(j, i)
+                // collides with the bottom or top wall
+                if(absPos.y > this.field.length-1 || absPos.y < 0) return true
+                // collides with the left or right wall
+                if(absPos.x < 0 || absPos.x > this.field[0].length-1) return true
+                // collides with anoteher shape
+                if(this.field[absPos.y][absPos.x].collision()) return true
+            }
+        }
+        return false
+    }
+
+    hideShape = () => {
+        for (let i = 0; i < this.currentShape.length; i++) {
+            for (let j = 0; j < this.currentShape[i].length; j++) {
+                let absPos = this.getPos(j, i)
+                if(this.currentShape[i][j] == 1) this.field[absPos.y][absPos.x].setColor('black')
+            }
+        }
+    }
+
+    renderShape = () => {
+        for (let i = 0; i < this.currentShape.length; i++) {
+            for (let j = 0; j < this.currentShape[i].length; j++) {
+                let absPos = this.getPos(j, i)
+                if(this.currentShape[i][j] == 1) this.field[absPos.y][absPos.x].setColor(this.shape.color)
+            }
+        }
+    }
+
     rotate = () => {
         if(this.rotation+1 == this.shape.shapes.length) this.rotation = 0
         else this.rotation++
 
+        this.hideShape()
+
         this.currentShape = this.shape.shapes[this.rotation]
+
+        if(this.checkCollision()) {
+            if(this.rotation == 0) this.rotation = this.shape.shapes.length-1
+            else this.rotation--
+            this.currentShape = this.shape.shapes[this.rotation]
+        }
+
+        this.renderShape()
     }
 
-    checkCollision = (field) => {
-        for (let i = 0; i < this.currentShape.length; i++) {
-            for (let j = 0; j < this.currentShape[i].length; j++) {
-                let absPos = this.getPos(j, i)
-                // collides with anoteher shape
-                if(this.currentShape[i][j] == 1 && field[absPos.y][absPos.x].collision()) return true
-                // collides with the bottom
-                if(this.currentShape[i][j] == 1 && absPos.y > field.length-1) return true
-            }
+    moveSideways = (left) => {
+        const lastPos = Object.assign({}, this.centerPos)
+
+        this.hideShape()
+
+        if(left) this.centerPos.x--
+        else this.centerPos.x++
+        
+        if(this.checkCollision()) {
+            this.centerPos = lastPos
         }
+
+        this.renderShape()
     }
 
-    hideShape = (field) => {
-        for (let i = 0; i < this.currentShape.length; i++) {
-            for (let j = 0; j < this.currentShape[i].length; j++) {
-                let absPos = this.getPos(j, i)
-                if(this.currentShape[i][j] == 1) field[absPos.y][absPos.x].setColor('black')
-            }
-        }
-    }
+    fall = () => {
+        // in case of collision save last position and shape
+        const lastPos = Object.assign({}, this.centerPos)
+        const lastShape = Object.assign([], this.currentShape)
 
-    renderShape = (field) => {
-        for (let i = 0; i < this.currentShape.length; i++) {
-            for (let j = 0; j < this.currentShape[i].length; j++) {
-                let absPos = this.getPos(j, i)
-                if(this.currentShape[i][j] == 1) field[absPos.y][absPos.x].setColor(this.shape.color)
-            }
+        this.hideShape()
+
+        // update position
+        this.centerPos.y++
+        
+        // check collision after updating position
+        if(this.checkCollision()) {
+            this.centerPos = lastPos
+            this.currentShape = lastShape
+            this.renderShape()
+            return false
         }
+
+        this.renderShape()
+        return true
     }
 }
 
 // ================================================================================================
 
 const Cell = class {
-    constructor(span) {
-        this.span = span
+    constructor() {
+        this.span = document.createElement('span')
+        this.span.classList.add('black')
         this.color = 'black'
     }
 
@@ -171,9 +230,6 @@ const Cell = class {
 
 // used for rendering the game field
 const Field = class {
-    // 2D array of spans
-    // TODO: remove this 🤷🏿
-    // playField = []
     // 2D array of `Cell` objects
     gameState = []
 
@@ -184,10 +240,6 @@ const Field = class {
         this.width = cols
         this.height = rows
 
-        // span
-        const span = document.createElement('span')
-        span.classList.add('black')
-
         // table
         const table = document.createElement('table')
         table.classList.add('field')
@@ -197,7 +249,7 @@ const Field = class {
             const row = document.createElement('tr')
             this.gameState[i] = []
             for (let j = 0; j < cols; j++) {
-                this.gameState[i][j] = new Cell(span.cloneNode())
+                this.gameState[i][j] = new Cell()
                 row.appendChild(this.gameState[i][j].span)
             }
             table.appendChild(row)
@@ -208,19 +260,30 @@ const Field = class {
 
 
 // ================================================================================================
+var gameOver = false
+var down = false
 
 const Game = class {
     // initialize field
     field = new Field(20, 10)
     score = 0
+    speed = 400
 
     // play game
     constructor() {
-        this.shape = new Shape(this.field.width)
+        this.score = 0
+        this.speed = 400
     }
 
-    play() {
-        return false
+    async play() {
+        while(!gameOver) {
+            this.shape = new Shape(this.field.gameState)
+            if(gameOver) break
+            this.shape.renderShape()
+            sleep(400)
+            while(this.shape.fall()) await sleep(this.speed)
+        }
+        
     }
 }
 
@@ -230,19 +293,9 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-
-
-
-
-
-
-
-
-
-
 var game = new Game()
-game.shape.renderShape(game.field.gameState)
 
+game.play()
 
 //field.playField[0][0].classList.add('yellow')
 //field.playField[0][0].className = 'black'
--- keyboard.js
+++ keyboard.js
@@ -1,24 +1,33 @@
+var wait = false
+
 window.onload = function (){
-    eventHandler = function (e){
+    eventHandler = async function (e){
         // left
-        if (e.keyCode == 37){
-            left = true;
-            console.log("left");
+        if(e.keyCode == 37){
+            console.log("left")
+            if(!gameOver) game.shape.moveSideways(true)
+        }
+        // up
+        else if(e.keyCode == 38){
+            console.log("up")
+            if(!gameOver) game.shape.rotate()
         }
         // right
-        else if (e.keyCode == 39){
-            right = true;
-            console.log("right");
+        else if(e.keyCode == 39){
+            console.log("right")
+            if(!gameOver) game.shape.moveSideways(false)
         }
         // down
         else if (e.keyCode == 40){
-            down = true;
-            console.log("down");
+            console.log("down")
+            if(!gameOver && !wait) {
+                wait = true
+                while(game.shape.fall()) await sleep(50)
+                game.shape = new Shape(game.field.gameState)
+                game.shape.renderShape()
+                wait = false
+            }
         }
     }
-    window.addEventListener('keydown', eventHandler, false);
-}
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+    window.addEventListener('keydown', eventHandler, false)
 }
