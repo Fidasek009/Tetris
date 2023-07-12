03012df3d44260a0abe145780a0f3bcf5b5805ee
fidasek009 <fidasek009@gmail.com>
2023-07-12 23:32:52 +0200

    🟥🟧🟨 TETRIS 🟩🟦🟪

--- keyboard.js
+++ keyboard.js
@@ -3,26 +3,43 @@ window.onload = function (){
         // left
         if(e.keyCode == 37){
             console.log("left")
-            if(!gameOver  && !wait && !paused) game.shape.moveSideways(true)
+            left()
         }
         // up
         else if(e.keyCode == 38){
             console.log("up")
-            if(!gameOver && !wait && !paused) game.shape.rotate()
+            rotate()
         }
         // right
         else if(e.keyCode == 39){
             console.log("right")
-            if(!gameOver  && !wait && !paused) game.shape.moveSideways(false)
+            right()
         }
         // down
         else if (e.keyCode == 40){
             console.log("down")
-            if(!gameOver  && !paused) {
-                game.speed = (game.speed > 50) ? 50 : 400
-                sleepInterrupt = true
-            }
+            drop()
         }
     }
     window.addEventListener('keydown', eventHandler, false)
+}
+
+
+const rotate = () => {
+    if(!gameOver && !wait && !paused) game.shape.rotate()
+}
+
+const drop = () => {
+    if(!gameOver  && !paused) {
+        game.speed = (game.speed > 50) ? 50 : 400
+        sleepInterrupt = true
+    }
+}
+
+const left = () => {
+    if(!gameOver  && !wait && !paused) game.shape.moveSideways(true)
+}
+
+const right = () => {
+    if(!gameOver  && !wait && !paused) game.shape.moveSideways(false)
 }
